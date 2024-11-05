49959278e3f8fb1fb6748bf10bb3fff6c91a9f70
Seongha Kim <hosh2326@naver.com>
2024-11-05 16:47:27 +0900

    (ver.1) 상하행 반대 수정, 음성페이지 js 추가

--- app/static/js/lotte2.js
+++ app/static/js/lotte2.js
@@ -1,3 +1,7 @@
+document.getElementById("home-btn").addEventListener("click", function () {
+  window.location.href = "lotte";
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   var doc = document.documentElement;
   var FullPage = document.getElementById("logo-image");
@@ -35,10 +39,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.getElementById("home-btn").addEventListener("click", function () {
-  window.location.href = "lotte";
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   var mapButton = document.querySelector(".map-button");
   var imgModal = document.querySelector(".img-modal");
@@ -46,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
   var buttonContainer = document.querySelector(".button-container");
   var modalImage = document.getElementById("modal-image");
 
-  // Function to set default state to 1F
   function setDefaultFloor() {
     var defaultButton = buttonContainer.querySelector("button[floor='1']");
     handleButtonClick({ target: defaultButton });
@@ -96,6 +95,10 @@ document.addEventListener("DOMContentLoaded", function () {
   var waveContainer = document.querySelector(".waveContainer");
   const modalImage = document.getElementById("store-modal-image");
   const imgModal = document.querySelector(".store-modal");
+  const cancelButton = document.querySelector(".store-modal .cancel-btn");
+  const GuideButton = document.querySelector(".store-modal .start-guide-btn");
+  const moveModal = document.querySelector(".movemodal");
+  const moveModalImage = document.getElementById("move-modal-image");
 
   function hideWaveContainer() {
     waveContainer.style.display = "none";
@@ -161,7 +164,15 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(matchedStoreData);
 
     if (matchedStoreData) {
-      const { store_floor, storeName, store_x, store_y } = matchedStoreData;
+      const {
+        store_floor,
+        storeName,
+        store_x,
+        store_y,
+        click_num,
+        upment,
+        downment,
+      } = matchedStoreData;
 
       if (modalImage && imgModal) {
         if (store_floor === "2F") {
@@ -178,6 +189,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         const marker = document.createElement("div");
+        const additionalMarker = document.createElement("div");
+
         marker.classList.add("store-marker");
         marker.style.position = "absolute";
         marker.style.width = "30px";
@@ -190,98 +203,219 @@ document.addEventListener("DOMContentLoaded", function () {
         marker.style.transform = "translate(-50%, -100%)";
         marker.style.pointerEvents = "none";
 
-        // 마커를 modalImage 위에 추가
         imgModal.appendChild(marker);
 
-        let audioFilePath;
-        switch (storeName) {
-          case "대화":
-            audioFilePath = "/static/audio/robot2.wav";
-            subtitleText =
-              "네! 고객님과 대화가 가능합니다. 어떤 것이 궁금하신가요?";
-            break;
-          case "어떤":
-            audioFilePath = "/static/audio/robot3.wav";
-            subtitleText =
-              "저는 고객님들께 매장 안내를 돕는 일을 합니다. 무엇을 도와드릴까요? 말씀해주세요.";
-            break;
-          case "bakery":
-            audioFilePath = "/static/audio/robot4.wav";
-            subtitleText =
-              "MaWang Pie is the most popular at this bakery. Try it.";
-            break;
-          case "톡톡히":
-            audioFilePath = "/static/audio/robot5.wav";
-            subtitleText = "고객님, 찾고 계시는 마왕파이가 여기 있습니다.";
-            break;
-          case "느낌입니다":
-            audioFilePath = "/static/audio/robot6.wav";
-            subtitleText = "고객님 더 필요한 것은 없나요?";
-            break;
-          case "고마워요":
-            audioFilePath = "/static/audio/robot7.wav";
-            subtitleText =
-              "그럼 좋은하루 되세요, 고객님. 저는 다른 곳으로 먼저 이동하겠습니다.";
-            break;
-          default:
-            audioFilePath = null;
+        if (storeName === "화장실") {
+          additionalMarker.classList.add("store-marker");
+          additionalMarker.style.position = "absolute";
+          additionalMarker.style.width = "30px";
+          additionalMarker.style.height = "30px";
+          additionalMarker.style.backgroundImage =
+            "url(/static/img/marker.png)";
+          additionalMarker.style.backgroundSize = "contain";
+          additionalMarker.style.backgroundRepeat = "no-repeat";
+          additionalMarker.style.left = "880px";
+          additionalMarker.style.top = "122px";
+          additionalMarker.style.transform = "translate(-50%, -100%)";
+          additionalMarker.style.pointerEvents = "none";
+
+          imgModal.appendChild(additionalMarker);
         }
 
-        if (audioFilePath) {
-          const audio = new Audio(audioFilePath);
-          audio.play();
-          console.log(
-            `매장명: ${storeName}, 층수: ${store_floor}, x_loc: ${store_x}, y_loc: ${store_y}`
+        let utterance;
+
+        if (storeName === "화장실") {
+          utterance = new SpeechSynthesisUtterance(
+            `화장실은 이 위치에 있습니다. 안내를 시작할까요?`
           );
-
-          imgModal.style.display = "none";
-
-          const overlayElement = document.createElement("div");
-          overlayElement.style.position = "fixed";
-          overlayElement.style.top = "0";
-          overlayElement.style.left = "0";
-          overlayElement.style.width = "100%";
-          overlayElement.style.height = "100%";
-          overlayElement.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-          overlayElement.style.zIndex = "999";
-
-          const subtitleElement = document.createElement("div");
-          subtitleElement.classList.add("subtitle");
-          subtitleElement.style.position = "absolute";
-          subtitleElement.style.width = "1000px";
-          subtitleElement.style.bottom = "30%";
-          subtitleElement.style.left = "50%";
-          subtitleElement.style.transform = "translateX(-50%)";
-          subtitleElement.style.padding = "10px";
-          subtitleElement.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-          subtitleElement.style.color = "#fff";
-          subtitleElement.style.fontSize = "36px";
-          subtitleElement.style.zIndex = "1000";
-          subtitleElement.style.textAlign = "center";
-          subtitleElement.innerText = subtitleText;
-
-          document.body.appendChild(overlayElement);
-          document.body.appendChild(subtitleElement);
-
-          setTimeout(() => {
-            subtitleElement.remove();
-            overlayElement.remove();
-          }, 8000);
         } else {
-          let utteranceText;
-          if (store_floor === "2F") {
-            utteranceText = `${storeName} 매장은 2층에 있습니다.`;
-          } else {
-            utteranceText = `${storeName} 매장으로 안내합니다.`;
+          utterance = new SpeechSynthesisUtterance(
+            `${storeName} 매장은 이 위치에 있습니다. 안내를 시작할까요?`
+          );
+        }
+
+        if (store_floor === "2F") {
+          utterance.text = `${storeName} 매장은 2층에 있습니다.`;
+        }
+
+        speechSynthesis.speak(utterance);
+
+        let previousClickNum = null;
+        let lastGoingValue = null;
+
+        function updateClickNumInDB(clickNum) {
+          if (previousClickNum === clickNum) {
+            return;
           }
 
-          const utterance = new SpeechSynthesisUtterance(utteranceText);
-          speechSynthesis.speak(utterance);
+          previousClickNum = clickNum;
+
+          fetch("/update_click_num", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ click_num: clickNum }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log("DB 업데이트 성공:", data);
+              checkGoingStatus();
+            })
+            .catch((error) => {
+              console.error("데이터 가져오기 실패:", error);
+            });
         }
 
-        setTimeout(() => {
+        function checkGoingStatus() {
+          const interval = setInterval(() => {
+            fetch("/get_going_status", {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(
+                  "현재 going 값:",
+                  data.going,
+                  "direction:",
+                  data.direction
+                );
+
+                let completionMessage;
+                speechSynthesis.cancel();
+
+                if (data.going === 0) {
+                  moveModal.style.display = "none";
+
+                  if (lastGoingValue !== 0) {
+                    lastGoingValue = data.going;
+                    //direction == 0 (상행), direction == 1 (하행)
+                    if (data.direction === 0 && upment) {
+                      completionMessage = new SpeechSynthesisUtterance(upment);
+                      console.log("멘트:", upment);
+                    } else if (data.direction === 1 && downment) {
+                      completionMessage = new SpeechSynthesisUtterance(
+                        downment
+                      );
+                      console.log("멘트:", downment);
+                    }
+
+                    if (completionMessage) {
+                      speechSynthesis.speak(completionMessage);
+                    } else {
+                      console.error("음성 메시지가 설정되지 않았습니다.");
+                    }
+
+                    clearInterval(interval);
+                  }
+                } else {
+                  lastGoingValue = data.going;
+                }
+              })
+              .catch((error) => {
+                console.error("데이터 가져오기 실패:", error);
+                clearInterval(interval);
+              });
+          }, 1000);
+        }
+        GuideButton.addEventListener("click", function () {
           imgModal.style.display = "none";
-        }, 10000);
+          marker.remove();
+          additionalMarker.remove();
+
+          const clickNum = click_num;
+
+          updateClickNumInDB(clickNum);
+
+          moveModalImage.src = "/static/img/movemodal.png";
+          moveModal.style.display = "block";
+        });
+
+        cancelButton.addEventListener("click", function () {
+          imgModal.style.display = "none";
+          marker.remove();
+          additionalMarker.remove();
+
+          hideWaveContainer();
+          recognition.stop();
+          micIcon.classList.remove("m-active");
+          micButtonLoader.classList.remove("active");
+          recognitionActive = false;
+        });
+
+        // let audioFilePath;
+        // switch(storeName) {
+        //     case '대화':
+        //         audioFilePath = '/static/audio/robot2.wav';
+        //         subtitleText = '네! 고객님과 대화가 가능합니다. 어떤 것이 궁금하신가요?';
+        //         break;
+        //     case '어떤':
+        //         audioFilePath = '/static/audio/robot3.wav';
+        //         subtitleText = '저는 고객님들께 매장 안내를 돕는 일을 합니다. 무엇을 도와드릴까요? 말씀해주세요.';
+        //         break;
+        //     case 'bakery':
+        //         audioFilePath = '/static/audio/robot4.wav';
+        //         subtitleText = 'MaWang Pie is the most popular at this bakery. Try it.';
+        //         break;
+        //     case '톡톡히':
+        //         audioFilePath = '/static/audio/robot5.wav';
+        //         subtitleText = '고객님, 찾고 계시는 마왕파이가 여기 있습니다.';
+        //         break;
+        //     case '느낌입니다':
+        //         audioFilePath = '/static/audio/robot6.wav';
+        //         subtitleText = '고객님 더 필요한 것은 없나요?'
+        //         break;
+        //     case '고마워요':
+        //         audioFilePath = '/static/audio/robot7.wav';
+        //         subtitleText = '그럼 좋은하루 되세요, 고객님. 저는 다른 곳으로 먼저 이동하겠습니다.';
+        //         break;
+        //     default:
+        //         audioFilePath = null;
+        // }
+
+        // if (audioFilePath) {
+        //     const audio = new Audio(audioFilePath);
+        //     audio.play();
+        //     console.log(`매장명: ${storeName}, 층수: ${store_floor}, x_loc: ${store_x}, y_loc: ${store_y}`);
+
+        //     imgModal.style.display = 'none';
+
+        //     const overlayElement = document.createElement('div');
+        //     overlayElement.style.position = 'fixed';
+        //     overlayElement.style.top = '0';
+        //     overlayElement.style.left = '0';
+        //     overlayElement.style.width = '100%';
+        //     overlayElement.style.height = '100%';
+        //     overlayElement.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        //     overlayElement.style.zIndex = '999';
+
+        //     const subtitleElement = document.createElement('div');
+        //     subtitleElement.classList.add('subtitle');
+        //     subtitleElement.style.position = 'absolute';
+        //     subtitleElement.style.width = '1000px'
+        //     subtitleElement.style.bottom = '30%';
+        //     subtitleElement.style.left = '50%';
+        //     subtitleElement.style.transform = 'translateX(-50%)';
+        //     subtitleElement.style.padding = '10px';
+        //     subtitleElement.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+        //     subtitleElement.style.color = '#fff';
+        //     subtitleElement.style.fontSize = '36px';
+        //     subtitleElement.style.zIndex = '1000';
+        //     subtitleElement.style.textAlign = 'center';
+        //     subtitleElement.innerText = subtitleText;
+
+        //     document.body.appendChild(overlayElement);
+        //     document.body.appendChild(subtitleElement);
+
+        //     setTimeout(() => {
+        //         subtitleElement.remove();
+        //         overlayElement.remove();
+        //     }, 8000);
+
+        // }
       } else {
         console.error("모달 이미지 또는 모달 요소를 찾을 수 없습니다.");
       }
@@ -403,7 +537,7 @@ function playWelcomeVoice() {
     return;
   }
 
-  var welcomeAudio = new Audio("/static/audio/robot1.wav");
+  var welcomeAudio = new Audio("/static/audio/welcome.wav");
   welcomeAudio.play();
 }
 
